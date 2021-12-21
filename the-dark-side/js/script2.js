a007615271cbaf3bf9cb87cfc49b8584bbcce974
Mezoo-Cs <92177398+Mezoo-Cs@users.noreply.github.com>
2021-12-21 15:57:24 +0200

    Add files via upload

---
+++ the-dark-side/js/script2.js
@@ -0,0 +1,15 @@
+let myWork = document.getElementById("my-work"), 
+    viewEle = document.querySelector(".view"),
+    gallary =[];
+
+    for( i = 0; i < 20; i++ ){
+        gallary[i] = document.createElement("div");
+        myWork.appendChild(gallary[i]);
+        gallary[i].style.backgroundImage = "url(../img/my-work-page-2/sa3aid-" + i +".jpg";
+    }
+    gallary.forEach(function(ele){
+        ele.addEventListener("click", () => {
+            viewEle.style.backgroundImage = ele.style.backgroundImage;
+        });
+    });
+
